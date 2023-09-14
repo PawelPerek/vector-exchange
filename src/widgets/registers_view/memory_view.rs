704550ee33977fd366fd7007a0c1cacaed90b522
Paweł Perek <pawelperek99@gmail.com>
2023-09-14 22:29:18 +0200

    Implement minimal memory view

--- src/widgets/registers_view/memory_view.rs
+++ src/widgets/registers_view/memory_view.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use eeric::prelude::*;
 use leptos::*;
 
@@ -10,13 +12,17 @@ pub fn MemoryView(cx: Scope) -> impl IntoView {
         state
             .read_core()
             .map(|machine| machine.memory.snapshot())
-            .unwrap_or_default()
+            .unwrap_or(VecDeque::from_iter((0..0x100).map(|_| 0)))
     });
 
     view! { cx, 
         <div class="bg-white rounded p-4 shadow-xl max-h-[75%] overflow-y-scroll">
             <h1 class="font-bold text-center border border-gray-200 py-6">Memory</h1>
-            <div class="grid grid-cols-[repeat(32,minmax(0,max-content))] px divide-x divide-y border border-gray-200">
+            <div class="grid grid-cols-[repeat(17,minmax(0,max-content))] px divide-x divide-y border border-gray-200 text-center">
+                <div class="bg-gray-100 font-bold">+</div>
+                {
+                    (0..0x10).map(|i| format!("{:x}", i)).map(|addr| view! {cx, <div class="w-10 bg-gray-100 font-bold">{addr.to_uppercase()}</div>}).collect_view(cx)
+                }
                 {move || {
                     memory()
                         .iter()
@@ -25,13 +31,19 @@ pub fn MemoryView(cx: Scope) -> impl IntoView {
                             view! {
                                 cx, 
                                 <>
-                                <div class="px-0.5 text-center">
+                                <Show 
+                                    when=move || address % 0x10 == 0
+                                    fallback=|_| ()
+                                >
+                                    <div class="w-12 bg-gray-100 font-bold">{format!("{:02x}", address).to_uppercase()}</div>
+                                </Show>
+                                <div>
                                     {byte.to_string()}
                                 </div>
                                 <>
                             }
                         })
-                        .collect::<Vec<_>>()
+                        .collect_view(cx)
                 }}
             </div>
         </div> }
